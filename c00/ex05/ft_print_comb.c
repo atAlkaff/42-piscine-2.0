1616bdb84ccd01f43a47b34b1819c64fc6154fc3
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2025-09-16 00:36:15 +0400

    c00 ex05 O(n) algorithm with one syscall

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalkaff <aalkaff@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/15 23:40:31 by aalkaff           #+#    #+#             */
+/*   Updated: 2025/09/16 00:35:29 by aalkaff          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+#define BUFFER_SIZE 600
+
+char	*load_number(char *buffer, unsigned int digits[3])
+{
+	buffer[0] = digits[0] + '0';
+	buffer[1] = digits[1] + '0';
+	buffer[2] = digits[2] + '0';
+	buffer[3] = ',';
+	buffer[4] = ' ';
+	return (buffer + 5);
+}
+
+void	ft_print_comb(void)
+{
+	char			buffer[BUFFER_SIZE];
+	char			*iterator;
+	unsigned int	digits[3];
+	unsigned int	number;
+
+	number = 12;
+	iterator = buffer;
+	while (number <= 789)
+	{
+		digits[0] = number / 100;
+		digits[1] = (number % 100) / 10;
+		digits[2] = number % 10;
+		iterator = load_number(iterator, digits);
+		if (digits[2] == 9)
+		{
+			if (digits[1] == 8)
+				number += 11 * digits[0] + 34;
+			else
+				number += digits[1] + 3;
+		}
+		else
+			number++;
+	}
+	write(STDOUT_FILENO, buffer, BUFFER_SIZE - 2);
+}
