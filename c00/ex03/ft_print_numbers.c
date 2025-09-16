7af492793e24a3f9e6f2cb1c852958a5e270e1f6
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2025-09-16 14:56:23 +0400

    fixed ft_print_numbers to write 10 bytes not 26

--- c00/ex03/ft_print_numbers.c
+++ c00/ex03/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: aalkaff <aalkaff@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 22:16:26 by aalkaff           #+#    #+#             */
-/*   Updated: 2025/09/15 22:17:22 by aalkaff          ###   ########.fr       */
+/*   Updated: 2025/09/16 14:55:57 by aalkaff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ void	ft_print_numbers(void)
 		numbers[i] = '0' + i;
 		i++;
 	}
-	write(STDOUT_FILENO, numbers, 26);
+	write(STDOUT_FILENO, numbers, 10);
 }
